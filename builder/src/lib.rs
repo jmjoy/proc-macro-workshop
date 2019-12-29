3d881a65ea97d79a1f8fe5d35cd0a1527e97f01a
__JM_Joy__ <918734043@qq.com>
2019-12-29 12:49:45 +0800

    完成builder第二道题。

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,50 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream};
+use quote::{quote, quote_spanned};
+use syn::{parse_macro_input, DeriveInput, Ident, Data, Fields, Type};
+use syn::token::Comma;
+use syn::export::Span;
+use syn::punctuated::Punctuated;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
 
-    unimplemented!()
+    let metadata = match &input.data {
+        Data::Struct(s) => match &s.fields {
+            Fields::Named(fields) => {
+                fields.named.iter().map(|field| {
+                    let ident = &field.ident;
+                    let ty = &field.ty;
+                    let expanded = quote! {
+                         #ident: Option<#ty>,
+                    };
+                    (ident, expanded)
+                })
+            }
+            _ => todo!(),
+        },
+        _ => todo!(),
+    };
+
+    let field_names = metadata.clone().map(|item| item.0);
+    let fields = metadata.map(|item| item.1);
+
+    let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
+    let expanded = quote! {
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#field_names: None,)*
+                }
+            }
+        }
+
+        pub struct #builder_name {
+            #(#fields)*
+        }
+    };
+    expanded.into()
 }
